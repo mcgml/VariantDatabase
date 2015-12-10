c9c2d81f92ef99e1152ef55e687bab77b7e2e763
mcgml <mcgml@cardiff.ac.uk>
2015-12-10 13:19:09 +0000

    fixed user info bug

--- app/account/account.js
+++ app/account/account.js
@@ -12,18 +12,17 @@ angular.module('variantdatabase.account', ['ngRoute', 'ui-notification'])
     .controller('AccountCtrl', ['$scope', '$http', 'Notification', function ($scope, $http, Notification) {
 
         function getUserInformation() {
-            $scope.getUserInformation = function(){
-                $http.post('/api/variantdatabase/getuserinformation',
-                    {
-                        'UserNodeId' : 0
-                    })
-                    .then(function(response) {
-                        $scope.userInformation = response.data;
-                    }, function(response) {
-                        Notification.error(response);
-                        console.log("ERROR: " + response);
-                    });
-            };
+            $http.post('/api/variantdatabase/getuserinformation',
+                {
+                    'UserNodeId' : 0
+                })
+                .then(function(response) {
+                    $scope.userInformation = response.data;
+                }, function(response) {
+                    Notification.error(response);
+                    console.log("ERROR: " + response);
+                });
         }
+
         getUserInformation();
     }]);
